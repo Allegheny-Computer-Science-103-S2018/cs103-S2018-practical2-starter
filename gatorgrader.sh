1c5c79cc8d00387796e2780e911363994d739d9d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-02-09 08:13:32 -0500

    Including the correct commit check in the gatorgrader script.

--- gatorgrader.sh
+++ gatorgrader.sh
@@ -161,7 +161,7 @@ if [ "$CHECK" = true ]; then
                                      --checkfiles answers.md --fragments "*" --fragmentcounts 3
   determine_exit_code $?
   # --> GatorGrader CHECK: the repository contains at least three commits (update to current + 3)
-  python3 gatorgrader/gatorgrader.py --nowelcome --commits 3
+  python3 gatorgrader/gatorgrader.py --nowelcome --commits 10
   determine_exit_code $?
   echo ""
   printf "%s\n" "${blu}... Finished checking with GatorGrader${end}"
